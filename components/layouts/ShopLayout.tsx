d30940fd11fba5ca3ecfb8a38a0403b3088a9359
Miguel Angel Ochoa <miguel.angel.8a.rivera@gmail.com>
2023-10-15 07:20:20 -0500

    start(S-8a): construccion inicial del Layout del shop

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import { FC } from 'react';
+
+interface IShopLayoutProps {
+  children?: React.ReactNode;
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+}
+
+export const ShopLayout: FC<IShopLayoutProps> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+        {/* metatags para compartir en redes sociales */}
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={pageDescription} />
+
+        {/* para compartir url de imagen en redes sociales */}
+
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+      <nav>{/*TODO: Navbar */}</nav>
+
+      {/* TODO: Sidebar*/}
+      <main
+        style={{
+          margin: '80px auto',
+          maxWidth: '1400px',
+          padding: '0px 30px',
+        }}
+      >
+        {children}
+      </main>
+
+      {/* Footer */}
+      <footer>{/* TODO: Footer custom */}</footer>
+    </>
+  );
+};
